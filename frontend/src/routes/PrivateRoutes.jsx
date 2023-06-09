09611886e00708a2bb07a5c36378eb3c6306ca5b
Gangaram1998 <ramggawade1998@gmail.com>
2023-06-09 12:51:47 +0530

    Admin

--- frontend/src/routes/PrivateRoutes.jsx
+++ frontend/src/routes/PrivateRoutes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, useLocation } from 'react-router-dom'
 
-const PrivateRoutes = () => {
-  return (
-    <div>PrivateRoutes</div>
-  )
+const PrivateRoute = ({ children }) => {
+    const isAuth = useSelector((store) => store.authReducer.isAuth)
+    
+    const location = useLocation();
+
+    if (!isAuth) {
+        return <Navigate to={'/login'} state={location.pathname} replace />;   
+    }
+    return children
 }
 
-export default PrivateRoutes
+export default PrivateRoute
