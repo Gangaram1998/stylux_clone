771bf1899e9aef40af128ce8b5a58ce846a1e3c0
Gangaram1998 <ramggawade1998@gmail.com>
2023-06-09 14:43:56 +0530

    products

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,271 @@
+import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import SlideShow from "../components/HomePage/SlideShow";
+import { CONTAINER } from "../constants/constants";
+import { BOLD, FILL_PARENT, R1, R2, R3, R4 } from "../constants/typography";
+import "../styles/styles.css";
+
+export default function Homepage() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <Box width={FILL_PARENT} m={"auto"} mt={CONTAINER}>
+      {/* 1st box  */}
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        w={"95%"}
+        gap={5}
+        m="auto"
+      >
+        <SlideShow  />
+
+        <Stack
+          // w={{ base: "95%", md: "40%" }}
+          direction={{ base: "row", md: "column" }}
+          gap={{ base: "5", md: "2" }}
+          justifyContent="center"
+        >
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401197/samples/styluxe/men_shoes_rhs1_16march_heqqon.jpg"
+              alt="shoes"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401219/samples/styluxe/men1_tees_rhs1_feb18_qaenbd.jpg"
+              alt="t-Shirt"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+        </Stack>
+      </Stack>
+
+      {/* offfers */}
+      <Box my={5}>
+        <Heading>Offers</Heading>
+      </Box>
+
+      <Box
+        display="grid"
+        gridTemplateColumns={{ base: R2, md: R3, lg: R4 }}
+        w={"95%"}
+        m="auto"
+        gap={3}
+      >
+        <Box>
+          <Link to={"/products?category=shirt"}>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401309/samples/styluxe/square_shirts_09022018_xrr8jy.jpg"
+              alt="shirt"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Link>
+        </Box>
+
+        <Box>
+          <Link to={"/products?category=top"}>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401326/samples/styluxe/square-tops_09022018_iqjqkg.jpg"
+              alt="top"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Link>
+        </Box>
+
+        <Box>
+        <Link to={"/products?category=Kurtis"}>
+          <Image
+            src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401343/samples/styluxe/square_kurtis_09022018_mvttwk.jpg"
+            alt="kurties"
+            transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+          />
+          </Link>
+        </Box>
+
+        <Box>
+        <Link to={"/products?category=party-wear"}>
+
+          <Image
+            src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401360/samples/styluxe/square_partywear_09022018_i7lyc1.jpg"
+            alt="mens-partywear"
+            transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+            </Link>
+        </Box>
+      </Box>
+
+      {/* 3rd div  */}
+
+      <Box
+        display={"grid"}
+        gridTemplateColumns={{ base: R1, md: R2, lg: R3 }}
+        m="auto"
+        gap={5}
+        mt={20}
+        mb={20}
+        w="95%"
+      >
+        <Stack m={"auto"} gap={4}>
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401377/samples/styluxe/men_bottomwear_10oct17_o0csg1.jpg"
+              alt="men-bottomwear"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401588/samples/styluxe/men_watches_10oct17_p9kwsh.jpg"
+              alt="men-watches"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+        </Stack>
+
+        <Stack gap={5} m={"auto"}>
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401611/samples/styluxe/center3-4-17_kywcqt.jpg"
+              alt="yepme-logotype"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+
+          <Box my={10}>
+            <Text fontSize={"25px"} fontWeight={BOLD}>
+              STYLE ALERT
+            </Text>
+          </Box>
+
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401652/samples/styluxe/center_images_winterwear_10oct17_qavqzj.gif"
+              alt="winter-wear"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+        </Stack>
+
+        <Stack m={"auto"} gap={4}>
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401685/samples/styluxe/women_bottomwear_10oct17_tbdxyo.jpg"
+              alt="women-bottomwear"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401702/samples/styluxe/women_watches_10oct17_huj2lp.jpg"
+              alt="women-watches"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+        </Stack>
+      </Box>
+
+      {/* 4th div  */}
+
+      <Box>
+        <Text fontWeight={"bold"} fontSize="25px">
+          {" "}
+          SPRING SHOP{" "}
+        </Text>
+        <Text my={4} fontSize="20px">
+          {" "}
+          Find the key pieces for a cool summer wardrobe{" "}
+        </Text>
+      </Box>
+
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        justifyContent={"center"}
+        m="auto"
+        w={"95%"}
+        gap={5}
+      >
+        <Box overflow={"hidden"}>
+          <Image src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401716/samples/styluxe/summer_fashion_14june_cjx5e1.jpg"  transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}/>
+        </Box>
+
+        <Stack direction={"row"} m="auto" gap={5}>
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401737/samples/styluxe/women_dresses_jumpsuits_10oct17_fibfya.gif"
+              alt="women-dresses"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+          <Box>
+            <Image
+              src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401748/samples/styluxe/activewear_10oct17_eu7t2l.gif"
+              alt="activewear"
+              transition={"transform 2s"}
+            _hover={{ transform: "scale(1.1)" }}
+            />
+          </Box>
+        </Stack>
+      </Stack>
+
+      {/* last div  */}
+
+      <Box mt={10}>
+        <Text fontWeight={"bold"} fontSize="25px">
+          {" "}
+          STYLE FEATURE{" "}
+        </Text>
+        <Text my={4} fontSize="20px">
+          Our Latest Videos, Trends, Fashion Editorials & More!{" "}
+        </Text>
+      </Box>
+
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        w="95%"
+        margin="auto"
+        justifyContent={"center"}
+      >
+        <Box w={{ base: "90%", md: "50%" }} m="auto">
+          <iframe
+            width="100%"
+            height="400px"
+            src="https://www.youtube.com/embed/ZId7sehdFoI"
+            title="Spring Summer Collection TVC - Unreasonably Fashionable TV Ad by Yepme"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            
+          ></iframe>
+        </Box>
+        <Box>
+          <Image
+            src="https://res.cloudinary.com/ducgyycpy/image/upload/v1677401768/samples/styluxe/STYLE-FEATURE_9FEB_zzueyi.jpg"
+            alt="style"
+          />
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,327 @@
+import {
+    Box,
+    Button,
+    filter,
+    Heading,
+    HStack,
+    Image,
+    Input,
+    SimpleGrid,
+    Skeleton,
+    Stack,
+    Text,
+  } from "@chakra-ui/react";
+  import axios from "axios";
+  import React, { useEffect, useState } from "react";
+  import { Link, useLocation, useSearchParams } from "react-router-dom";
+  import Paginantion from "../Admin/components/Paginantion/Paginantion";
+  import DrawerPro from "../components/ProductsPage/Drawer";
+  import SideBar from "../components/ProductsPage/SideBar";
+  import { BASE_URL } from "../constants/config";
+  import { CONTAINER } from "../constants/constants";
+  import { NONE } from "../constants/typography";
+  
+  const ProductPage = () => {
+  
+    let [productlist, setproductlist] = useState([]);
+    let [fulldata,setfullData]= useState([]);
+    let [isError, setisError] = useState(false);
+    let [isloading, setisloading] = useState(false);
+    let [griddata, setgriddata] = useState("");
+    let [count, setcount] = useState(0);
+    const [page, setPage] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+    let [query,setQuery]= useState("");
+  
+  let [searchParam,setSearchParam] = useSearchParams()
+  
+  
+  
+  useEffect(() => {
+          window.scrollTo(0, 0);
+        }, [page]);
+  
+    const [filter, setFilter] = useState({
+      price: {
+        max: false,
+        min: false,
+      },
+      color: {
+        black: false,
+        gray: false,
+        red: false,
+        yellow: false,
+        orange: false,
+        white: false,
+        pink: false,
+        purple: false,
+        blue: false,
+        green: false
+      },
+      material: {
+        cotton: false,
+        rayon: false,
+        polyester: false,
+        naylon: false
+      },
+      occasion: {
+        casual: false,
+        party: false,
+        formal: false,
+        semiformal: false
+      },
+      brand: {
+        levis: false,
+        vanheusen: false,
+        yepme: false,
+        zara: false,
+        mufti:false
+      },
+    });
+  
+    const search = useLocation().search;
+    const catg = new URLSearchParams(search).get("category");
+    
+    
+  
+    
+    //filtered object of object
+    function findTrueValues(data) {
+      const trueValues = {};
+      for (const key in data) {
+        for (const subKey in data[key]) {
+          if (data[key][subKey]) {
+            trueValues[key] = subKey;
+          }
+        }
+      }
+      return trueValues;
+    }
+  
+    
+   let finalFilter= findTrueValues(filter)
+  
+  const searchParams = new URLSearchParams(finalFilter);
+  const queryString = searchParams.toString();
+  
+  
+  
+  //search box.....
+  const handleInputChange = (event) => {
+    const query = event.target.value.toLowerCase();
+    setQuery(query);
+  
+  if(!query){
+    setproductlist(fulldata)
+  }else{
+  
+    const filteredData = productlist.filter(item => item.title.includes(query));
+    setproductlist(filteredData);
+  
+  }
+  
+    
+  }
+  
+  
+    let getdata = async (page) => {
+      try {
+        setisloading(true);
+        let res = await axios.get(
+          `${BASE_URL}/product?category=${catg}&page=${page}&${queryString}`
+        );
+        //?gender=female ya kuch bhi filter krna ha too
+        setproductlist(res.data.data);
+        setfullData(res.data.data)
+        setTotalPage(res.data.count);
+        setisloading(false);
+      } catch (err) {
+        setisError(true);
+      }
+    };
+  
+    useEffect(() => {
+      getdata(page);
+    }, [page, filter,searchParam]);
+  
+    useEffect(() => {}, [griddata]);
+  
+  
+  
+    let handleHigh = () => {
+      setcount(count + 1);
+      let highdata = productlist.sort((a, b) => {
+        return +b.price - +a.price;
+      });
+  
+      setproductlist(highdata);
+    };
+  
+    let handleLow = () => {
+      setcount(count + 1);
+      let lowdata = productlist.sort((a, b) => {
+        return +a.price - +b.price;
+      });
+  
+      setproductlist(lowdata);
+    };
+  
+    
+    useEffect(() =>{
+      setSearchParam({category: `${catg}` ,page:page+1 , ...finalFilter })
+    
+    },[page, filter])
+  
+  
+  
+    if (isloading) {
+      return (
+        <SimpleGrid
+          m="auto"
+          w="95%"
+          pt={10}
+          columns={{ base: 1, md: 2, lg: 3 }}
+          gap={15}
+        >
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+          <Skeleton height="500px" />
+        </SimpleGrid>
+      );
+    }
+  
+    return (
+      <Box mt={CONTAINER}>
+        {isError !== "" && <h1>{isError}</h1>}
+  
+        <Stack direction={{ base: "column", md: "row" }}>
+          {/* //filter part is here  */}
+          <Box
+            display={{ base: "none", lg: "block" }}
+            maxw={"50%"}
+            minW={"30%"}
+            // border="5px solid"
+            boxShadow="rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px"
+          >
+  
+            <SideBar filter={filter} setFilter={setFilter} />
+          </Box>
+  
+          <Box display={{ base: "block", lg: "none" }} pt={5}  w={"20%"} >
+            <DrawerPro filter={filter} setFilter={setFilter} />
+          </Box>
+  
+          {/* //data is shown in SimpleGrid  */}
+          <Box>
+            <Stack
+              gap={5}
+              direction={{ base: "column", md: "row" }}
+              px={12}
+              mb={5}
+              justifyContent={"space-between"}
+            >
+              <Box>
+                <Heading textAlign={"left"}>{catg.toUpperCase()}</Heading>
+              </Box>
+  
+              <HStack gap={5}>
+                <Button onClick={handleHigh}> High to Low </Button>
+                <Button onClick={handleLow}> Low to High </Button>
+              </HStack>
+  
+              <HStack gap={4} display={{ base: NONE, lg: "block" }}>
+                <Text fontWeight={"bold"}> Shows </Text>
+                <Button onClick={() => setgriddata(2)}> 2 </Button>
+                <Button onClick={() => setgriddata(3)}> 3 </Button>
+                <Button onClick={() => setgriddata(4)}> 4 </Button>
+              </HStack>
+            </Stack>
+  
+       <Stack my={5} >
+       <Input w={"90%"} m="auto" type="text" value={query} onChange={handleInputChange} placeholder="Search Items by Title. . . . . . . ." />
+       </Stack>
+  
+  
+            <SimpleGrid
+              columns={
+                griddata !== ""
+                  ? { base: 1, md: 2, lg: griddata }
+                  : { base: 1, md: 2, lg: 3 }
+              }
+              gap={10}
+              w="90%"
+              m={"auto"}
+            >
+              {productlist.map((el) => (
+                <Stack
+                  key={el._id}
+                  textAlign="left"
+                  boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+                  transition={"transform 2s"}
+                  _hover={{ transform: "scale(1.1)", border: "2px dotted black" }}
+                >
+                  <Link to={`/products/${el._id}`}>
+                    <Image src={el.image.split(",")[0]} />
+                  </Link>
+  
+                  <Stack p={5}>
+                    <Heading>
+                      {el.title.charAt(0).toUpperCase() + el.title.slice(1)}{" "}
+                    </Heading>
+                    <Text fontSize={"20px"} fontWeight="bold">
+                      {" "}
+                      Price: ₹ {el.price}
+                    </Text>
+                    <HStack
+                      display={"flex"}
+                      justifyContent="center"
+                      gap={3}
+                      m="auto"
+                    >
+                      {el.sizes.split(",").map((e) => (
+                        <Box
+                        key={100000 + Math.floor(Math.random() * 900000)}
+                          borderRadius={"5px"}
+                          border={"2px solid"}
+                          px={2}
+                          bg={"grey"}
+                          color="white"
+                        >
+                          {" "}
+                          {e}
+                        </Box>
+                      ))}
+                    </HStack>
+                  </Stack>
+                </Stack>
+              ))}
+            </SimpleGrid>
+          </Box>
+        </Stack>
+  
+        {/* pagination   */}
+  
+        <Paginantion
+          page={page}
+          setPage={setPage}
+          divide={10}
+          totalPage={totalPage}
+        />
+      </Box>
+    );
+  };
+  
+  export default ProductPage;
