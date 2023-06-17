4b2fdba1584fae6199ca3f23598b2a8a0540cadb
Gangaram1998 <ramggawade1998@gmail.com>
2023-06-17 11:46:04 +0530

    add

--- backend/middlewares/authenticator.js
+++ backend/middlewares/authenticator.js
@@ -1,43 +1,38 @@
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
-async function authenticator(req,res,next){
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+function authenticator(req, res, next) {
+  let token = req.headers.authorization;
 
-    let token = req.headers.authorization
-    jwt.verify(token, process.env.SecretKey, async function(err, decoded) {
-
-        if(err) res.send({
-            message:"Something went wrong: "+err,
-            status:0,
-            error:true
-        })
-
-        if(decoded){
-            console.log(decoded)
-            if(decoded.role=="deactivate"){
-                res.send({
-                    message:"Your Account is deactivated : Conatact SuperAdmin",
-                    status:0,
-                    error:true
-                })
-            }else{
-                next()
-            }
-
-
-        }else{
-            res.send({
-                message:"Invalid token , Please Login",
-                status:2,
-                error:true
-            })
-        }
-     
+  jwt.verify(token, process.env.SecretKey, function (err, decoded) {
+    if (err) {
+      return res.status(500).json({
+        message: "Something went wrong: " + err,
+        status: 0,
+        error: true,
       });
+    }
 
-
+    if (decoded) {
+      console.log(decoded);
+      if (decoded.role === "deactivate") {
+        return res.status(403).json({
+          message: "Your Account is deactivated: Contact SuperAdmin",
+          status: 0,
+          error: true,
+        });
+      }
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Invalid token, Please Login",
+        status: 2,
+        error: true,
+      });
+    }
+  });
 }
 
-module.exports={
-    authenticator
-}
+module.exports = {
+  authenticator,
+};
